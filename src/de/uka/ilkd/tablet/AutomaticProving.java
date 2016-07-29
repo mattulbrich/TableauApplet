32649f6999f6d8352ba4c53a798fbeb465dbf3eb
Mattias Ulbrich <ulbrich@kit.edu>
2016-07-29 19:06:33 +0200

    Adding a license header.

--- src/de/uka/ilkd/tablet/AutomaticProving.java
+++ src/de/uka/ilkd/tablet/AutomaticProving.java
@@ -1,3 +1,22 @@
+/* This file is part of TableauApplet.
+ *
+ * It has been written by Mattias Ulbrich <ulbrich@kit.edu>, 
+ * Karlsruhe Institute of Technology, Germany.
+ *
+ * TableauApplet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TableauApplet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TableauApplet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.uka.ilkd.tablet;
 
 import de.uka.ilkd.tablet.ChoiceItem.Close;
